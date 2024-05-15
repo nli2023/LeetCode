efd960d037116de72eda76bb7122529b34ac05b4
nli2023 <140079467+nli2023@users.noreply.github.com>
2024-05-15 15:01:25 +0930

    Solved LeetCode No. 344 Reverse String
    Solved LeetCode No. 344 Reverse String

---
+++ JAVA/Easy/ReverseString.java
@@ -0,0 +1,14 @@
+// LeetCode 344
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length - 1;
+        while (l < r) {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
